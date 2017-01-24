3b609ea5e2c69547f6a94bcfc5936fdc438ebf54
Admin <admin@mac-admin.local>
2017-01-24 14:54:37 +0300

    still not working at all

---
+++ lab/CustomCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCollectionViewCell.swift
+//  lab
+//
+//  Created by Admin on 24.01.17.
+//  Copyright © 2017 swiftlabs. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
---
+++ lab/FullCollectionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lab
+//
+//  Created by Admin on 24.01.17.
+//  Copyright © 2017 swiftlabs. All rights reserved.
+//
+
+import UIKit
+
+class FullCollectionViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
